5502e2dc923414429d0934fb2ece5fe239e84834
danielcidc <cidcdaniel8@gmail.com>
2023-05-16 21:08:25 -0300

    adição de um título, subtítulo e imagem centralizados em um retângulo azul

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 export default function Home(){
+
   return(
-    <div>
-      <h1>Dados Telemétricos</h1>
-      <h2>Título dois</h2>
+    <div className='bg-blue-700 flex flex-col items-center'>
+      <h1 className='text-7xl text-yellow-400'>DADOS TELEMÉTRICOS</h1>
+      <h2 className='text-2xl text-yellow-300'>dados importantes para a leitura remota do barco</h2>
+
+      <img
+        src='https://media.licdn.com/dms/image/C4E0BAQEz53FLbmnA6w/company-logo_200_200/0/1627007835135?
+        e=2147483647&v=beta&t=A9ljiHIx2eaFAMOfECbG3t6CYYC5UjtnjoLZAVMPtsc'
+        width={50}
+        height={50}
+      />
     </div>
   )
 
