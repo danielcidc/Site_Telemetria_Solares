0fd106880f5786aaed4057d6ea1293f463a00841
danielcidc <cidcdaniel8@gmail.com>
2023-05-17 23:52:26 -0300

    adição de uma imagem de fundo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,21 @@ export default function Home(){
         dados importantes para a leitura remota do barco
       </h2>
 
+      <div className='relative w-full h-screen flex grow'>
+        <img
+         src='https://essolar.com.br/wp-content/uploads/2020/05/essolar1305.png'
+         alt='Imagem de Fundo'
+         className='absolute inset-0 w-full h-full object-cover'
+         width={1000}
+         />
+      </div>
+
       <img
         src='https://media.licdn.com/dms/image/C4E0BAQEz53FLbmnA6w/company-logo_200_200/0/1627007835135?e=2147483647&v=beta&t=A9ljiHIx2eaFAMOfECbG3t6CYYC5UjtnjoLZAVMPtsc'
         alt='Logo Solares'
-        style={{position: 'fixed', top: 0, left: 0}}
-        width={123}
-        height={100}
+        className='absolute top-0 left-0'
+        style={{ width: 140, height: 124}}
+        
       />
     </div>
   );
