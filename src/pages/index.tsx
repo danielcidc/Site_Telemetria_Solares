8235c83bcc5f525c91403c582607eaa54ab28a31
danielcidc <cidcdaniel8@gmail.com>
2023-05-18 00:56:34 -0300

    quadro central para exibir os dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home(){
         DADOS TELEMÉTRICOS
       </h1>
 
-      <h2 className='text-2xl text-yellow-100 font-montserrat mb-2'
+     <h2 className='text-2xl text-yellow-100 font-montserrat mb-2'
       style={{textShadow: '2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000'}}>
         dados importantes para a leitura remota do barco
       </h2>
@@ -19,17 +19,25 @@ export default function Home(){
          src='https://essolar.com.br/wp-content/uploads/2020/05/essolar1305.png'
          alt='Imagem de Fundo'
          className='absolute inset-0 w-full h-full object-cover'
-         width={1000}
          />
+
+       <div className='absolute inset-0 flex items-center justify-center'>
+        <img
+          src='https://img.freepik.com/vetores-premium/ilustracao-de-quadro-verde_9845-457.jpg'
+          alt='Quadro de exibição dos dados'
+          className='w-3/5 h-4/5'
+        />
       </div>
 
+     <div style={{ position: 'absolute', top: -124, left:0}}>
       <img
         src='https://media.licdn.com/dms/image/C4E0BAQEz53FLbmnA6w/company-logo_200_200/0/1627007835135?e=2147483647&v=beta&t=A9ljiHIx2eaFAMOfECbG3t6CYYC5UjtnjoLZAVMPtsc'
         alt='Logo Solares'
-        className='absolute top-0 left-0'
         style={{ width: 140, height: 124}}
         
       />
+      </div>
+     </div>
     </div>
   );
 
